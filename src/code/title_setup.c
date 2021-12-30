d49d4467679b37fdb5ad24eff0cd0ceaa65c0473
Dragorn421 <Dragorn421@users.noreply.github.com>
2021-12-30 14:26:14 +0100

    Load to gameplay right after boot

--- src/code/title_setup.c
+++ src/code/title_setup.c
@@ -3,8 +3,22 @@
 void Setup_InitImpl(SetupState* this) {
     osSyncPrintf("ゼルダ共通データ初期化\n"); // "Zelda common data initalization"
     SaveContext_Init();
+
+    /*
+    // vanilla
     this->state.running = false;
     SET_NEXT_GAMESTATE(&this->state, ConsoleLogo_Init, ConsoleLogoState);
+    */
+
+    this->state.running = false;
+    gSaveContext.gameMode = GAMEMODE_NORMAL;
+    gSaveContext.linkAge = LINK_AGE_ADULT;
+    Sram_InitDebugSave();
+    gSaveContext.dayTime = CLOCK_TIME(12, 0);
+    gSaveContext.entranceIndex = ENTR_SPOT00_0;
+    gSaveContext.respawnFlag = 0;
+    gSaveContext.respawn[RESPAWN_MODE_DOWN].entranceIndex = ENTR_LOAD_OPENING;
+    SET_NEXT_GAMESTATE(&this->state, Play_Init, PlayState);
 }
 
 void Setup_Destroy(GameState* thisx) {
